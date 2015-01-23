751f51d00974a30f2a141534d0af1525c03116b0
sekulic <sekulic.radosav@yahoo.com>
2015-01-23 23:27:02 +0100

    making some structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def is_active_page?(link_path)
+    if current_page?(link_path)
+      return "active" if request.query_parameters.empty?     
+    else
+      ""
+    end    
+  end
 end
