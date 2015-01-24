0204be254451b2847b153bab2f643ba0bc8c1a4c
sekulic <sekulic.radosav@yahoo.com>
2015-01-24 23:46:46 +0100

    cahnge models rent and car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ActiveRecord::Base
   belongs_to :comfort_type
+  has_many :rents
   validates_presence_of :name, :description, :comfort_type_id
   validates :photo1, :photo2, :photo3, :attachment_presence => true
   has_attached_file :photo1, :styles => { :medium => "640x480>", :thumb => "160x120>" }
